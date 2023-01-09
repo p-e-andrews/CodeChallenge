865036d25cdc62b79dde04c7d0dd3f40cfbf7b58
Phoebe <peandrews@my.waketech.edu>
2023-01-09 11:36:13 -0500

    adding basic working version of Code Challenge

---
+++ app/Http/Controllers/ProcessController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Process;
+
+class ProcessController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $processes = Process::all();
+
+        return view('processes.index', compact('processes'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('processes.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'=>'required',
+            'code'=>'required'
+        ]);
+
+        $process = new Process([
+            'id' => $request->get('id'),
+            'name' => $request->get('name'),
+            'description' => $request->get('description'),
+            'code' => $request->get('code'),
+            //'status' => $request->get('status');
+            'status' => $request->get('status') ? true : false
+        ]);
+        $process->save();
+        return redirect('/processes')->with('success', 'Process saved!');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $process = Process::find($id);
+        return view('processes.edit', compact('process'));        
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name'=>'required',
+            'code'=>'required'
+        ]);
+
+        $process = Process::find($id);
+        $process->name =  $request->get('name');
+        $process->description = $request->get('description');
+        $process->code = $request->get('code');
+        $process->status = $request->get('status');
+        $process->save();
+
+        return redirect('/processes')->with('success', 'Process updated!');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $process = Process::find($id);
+        $process->delete();
+
+        return redirect('/processes')->with('success', 'Process deleted!');
+
+    }
+}
---
+++ resources/views/processes/index.blade.php
@@ -0,0 +1,53 @@
+@extends('base')
+
+@section('main')
+<div class="row">
+<div class="col-sm-12">
+    <h1 class="display-3">Processes</h1>
+    <div>
+    <a style="margin: 19px;" href="{{ route('processes.create')}}" class="btn btn-primary">New process</a>
+    </div>    
+    <div class="col-sm-12">
+
+  @if(session()->get('success'))
+    <div class="alert alert-success">
+      {{ session()->get('success') }}  
+    </div>
+  @endif
+</div> 
+  <table class="table table-striped">
+    <thead>
+        <tr>
+          <td>UUID</td>
+          <td>Name</td>
+          <td>Description</td>
+          <td>Code</td>
+          <td>Status</td>
+          <td colspan = 2>Actions</td>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($processes as $process)
+        <tr>
+            <td>{{$process->id}}</td>
+            <td>{{$process->name}}</td>
+            <td>{{$process->description}}</td>
+            <td>{{$process->code}}</td>
+            <td>{{$process->status}}</td>
+            <td>
+                <a href="{{ route('processes.edit',$process->id)}}" class="btn btn-primary">Edit</a>
+            </td>
+            <td>
+                <form action="{{ route('processes.destroy', $process->id)}}" method="post">
+                  @csrf
+                  @method('DELETE')
+                  <button class="btn btn-danger" type="submit">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+  </table>
+<div>
+</div>
+@endsection
